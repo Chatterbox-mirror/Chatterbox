fd84b414699f67e745b1fe2c3c41ef26dcd30f0a
Dylandy Chang <steven80919@gmail.com>
2014-09-03 02:04:09 +0800

    add animation to new button and modified filter size

--- public/front_end/js/index.js
+++ public/front_end/js/index.js
@@ -1,3 +1,6 @@
 jQuery(function($){
   $('#page1 , #page2').css('height' , $(window).height() + 'px');
+  $('#more').click(function(){
+    $('html , body').animate({scrollTop: $('#page2').offset().top} , 800);
+  });
 });
