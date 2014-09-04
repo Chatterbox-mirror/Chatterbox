a17f90bcadbe05d17701b72227c576a4d1b02b2c
dylandy.chang <steven80919@gmail.com>
2014-09-04 11:37:16 +0800

    add column to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828031640) do
+ActiveRecord::Schema.define(version: 20140904033310) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140828031640) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "icon"
+    t.string   "categories"
   end
 
   add_index "groups", ["owner_id"], name: "index_groups_on_owner_id"
